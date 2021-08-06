b711a5c8235c97cb250c8c0795f7d005677bfd00
aymeric-spiga <aymeric.spiga@upmc.fr>
2021-08-06 11:38:52 +0200

    initial commit. analysis for constant-Hs simulation.

---
+++ les.py
@@ -0,0 +1,139 @@
+#! /usr/bin/env python
+from ppclass import pp
+import ppplot
+import numpy as np
+from ppcompute import smooth2diter
+
+## options
+from optparse import OptionParser ### TBR by argparse
+parser = OptionParser()
+parser.usage = "les.py netCDF_file(s)" #les.py becomes a commandline applicable to a netcdf file
+(opt,files) = parser.parse_args()
+
+### constants
+#t0 = 220.
+#p0 = 610.
+#r_cp = 1.0/4.4
+#grav = 3.72
+#R = 192.
+
+t0 = 180.
+p0 = 1.5e5
+r_cp = 0.28758
+grav = 1.35
+R = 299.
+
+## dimensions
+foo1,foo2,foo3,z,t = pp(file=files[0],var="T",x=0,y=0).getfd()
+nz,nt = z.size,t.size
+nf = len(files)
+print nf
+ntt = nt*nf
+
+## arrays for time series
+tprimemean,tmean = np.zeros([ntt,nz]),np.zeros([ntt,nz])
+wprimemean,wmean = np.zeros([ntt,nz]),np.zeros([ntt,nz])
+wmax = np.zeros([ntt,nz])
+vehfmean = np.zeros([ntt,nz])
+pblh,pblh1,pblh2,pblh3 = np.zeros(ntt),np.zeros(ntt),np.zeros(ntt),np.zeros(ntt)
+
+## loop
+indt = -1
+
+for ff in files:
+
+ print "### PROCESSING FILE:",ff
+ print "### --> T"
+ t = pp(file=ff,var="T").getf() + t0
+ print "### --> W"
+ w = pp(file=ff,var="W").getf()
+ print "### --> PHTOT"
+ geop = pp(file=ff,var="PHTOT").getf()
+ geop = geop - np.mean(geop[:,0,:,:])
+ 
+ for tt in range(nt):
+
+  indt = indt + 1
+ 
+  for zz in range(nz):
+    #print tt, indt
+    tprime = t[tt,zz,:,:]
+    tmean[indt,zz] = np.mean(tprime)
+    tprime = tprime - tmean[indt,zz]
+ 
+    wprime = w[tt,zz,:,:]
+    wmean[indt,zz] = np.mean(wprime)
+    wmax[indt,zz] = np.max(np.abs(wprime))
+    wprime = wprime - wmean[indt,zz]
+ 
+    vehfmean[indt,zz] = np.mean(tprime*wprime)
+    tprimemean[indt,zz] = np.mean(tprime)
+    wprimemean[indt,zz] = np.mean(wprime)
+
+
+
+#  ###########
+#  ## method 1: potential temperature profile
+#  diff = np.abs(tmean[indt,2:]-tmean[indt,1])
+#  wheremin = np.argmin(diff) + 2
+#  #print indt, tt
+#  pblh1[indt] = np.mean(geop[tt,wheremin,:,:]) / grav #Height of the geopotential of the boundary layer
+ 
+  ###########
+  ## method 2: minimum of vertical eddy heat flux
+  wheremin = np.argmin(vehfmean[indt,:]) 
+  pblh2[indt] = 1.1*np.mean(geop[tt,wheremin,:,:]) / grav
+  ### sometimes spurious values caused by GW
+  #diff = 100.*np.abs(pblh2[indt]-pblh1[indt])/pblh1[indt]
+  #if diff > 33.: pblh2[indt] = pblh1[indt]
+
+#  ###########
+#  ## method 3: convective motions
+#  diff = np.abs(wmax[indt,8:]-np.max(wmax[indt,:]/3.))
+#  wheremin = np.argmin(diff) + 8
+#  pblh3[indt] = np.mean(geop[tt,wheremin,:,:]) / grav
+#  ## sometimes spurious values caused by GW
+#  diff = 100.*np.abs(pblh3[indt]-pblh1[indt])/pblh1[indt]
+#  if diff > 33.: pblh3[indt] = pblh1[indt]
+
+#  pblh[indt] = np.mean([pblh1[indt],pblh2[indt],pblh3[indt]])
+  pblh[indt] = pblh2[indt]
+
+### remove small or negative values 
+#pblh = pblh[pblh > 100.]
+
+## compute mean height
+altitude = np.mean(np.mean(np.mean(geop,axis=3),axis=2),axis=0)/grav
+altitude = altitude[0:nz]
+
+file1=open('pbl.txt','w')
+for val in pblh:
+  file1.write("%8.3f\n"%(val))
+file1.close()
+
+
+## 1 Titan day == nearly 16 Earth days
+## 1 Titan hour == 16/24 Earth days == 3600*24*16/24 == 3600*16
+pl = ppplot.plot1d() # plot of the boundary layer height time evolution
+pl.f = pblh
+pl.makeshow()
+pl = ppplot.plot2d() # shade of the vertical eddy heat flux time evolution
+pl.f = np.transpose(vehfmean[30:,:])*1e5
+pl.y = altitude/1000.
+nnn = np.shape(pl.f)[1]
+print nnn
+pl.x = np.arange(nnn)*400./(3600.*16.)
+pl.vmax = 15
+pl.vmin = -pl.vmax
+pl.div = 30
+pl.fmt = '%.0f'
+pl.ymax = 3.
+pl.ylabel = "altitude (km)"
+pl.xlabel = "simulated time (Titan hours)"
+pl.nyticks = 10
+pl.colorbar = "seismic"
+pl.units = r'10$^{-5}$ K m s$^{-1}$'
+
+#pl.makeshow()
+pl.makesave(mode="png",filename="vehf")
+
