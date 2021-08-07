65875f4611890c784d8382b0f2aa0e5fd545ef37
Aymeric Spiga <aymeric.spiga@lmd.jussieu.fr>
2021-08-07 17:20:24 +0200

    reworked version of les.py which now creates text files of PBL diagnostics

--- les.py
+++ les.py
@@ -10,6 +10,23 @@ parser = OptionParser()
 parser.usage = "les.py netCDF_file(s)" #les.py becomes a commandline applicable to a netcdf file
 (opt,files) = parser.parse_args()
 
+recalculate = False
+recalculate = True
+
+
+nfilespinup = 2.
+foutput = 400.
+noutput = 100.
+startlt = 6.
+titanhour = 3600.*16.
+## 1 Titan day == nearly 16 Earth days
+## 1 Titan hour == 16/24 Earth days == 3600*24*16/24 == 3600*16
+
+
+limpercent = 50.
+#####
+
+
 ### constants
 #t0 = 220.
 #p0 = 610.
@@ -17,123 +34,186 @@ parser.usage = "les.py netCDF_file(s)" #les.py becomes a commandline applicable 
 #grav = 3.72
 #R = 192.
 
-t0 = 180.
-p0 = 1.5e5
-r_cp = 0.28758
-grav = 1.35
-R = 299.
+#r_cp = 0.28758
+#R = 299.
 
-## dimensions
-foo1,foo2,foo3,z,t = pp(file=files[0],var="T",x=0,y=0).getfd()
-nz,nt = z.size,t.size
-nf = len(files)
-print nf
-ntt = nt*nf
+### from module_model_constants
+g            = 1.35
+r_d          = 290.6
+cp           = 1051.
+t0           = 94.
+p0           = 1.45e5
+p1000mb      = 1.45e5
+reradius     = 1./2575500.0
+wdaysec      = 1377665.28
 
-## arrays for time series
-tprimemean,tmean = np.zeros([ntt,nz]),np.zeros([ntt,nz])
-wprimemean,wmean = np.zeros([ntt,nz]),np.zeros([ntt,nz])
-wmax = np.zeros([ntt,nz])
-vehfmean = np.zeros([ntt,nz])
-pblh,pblh1,pblh2,pblh3 = np.zeros(ntt),np.zeros(ntt),np.zeros(ntt),np.zeros(ntt)
-
-## loop
-indt = -1
-
-for ff in files:
-
- print "### PROCESSING FILE:",ff
- print "### --> T"
- t = pp(file=ff,var="T").getf() + t0
- print "### --> W"
- w = pp(file=ff,var="W").getf()
- print "### --> PHTOT"
- geop = pp(file=ff,var="PHTOT").getf()
- geop = geop - np.mean(geop[:,0,:,:])
- 
- for tt in range(nt):
-
-  indt = indt + 1
- 
-  for zz in range(nz):
-    #print tt, indt
-    tprime = t[tt,zz,:,:]
-    tmean[indt,zz] = np.mean(tprime)
-    tprime = tprime - tmean[indt,zz]
- 
-    wprime = w[tt,zz,:,:]
-    wmean[indt,zz] = np.mean(wprime)
-    wmax[indt,zz] = np.max(np.abs(wprime))
-    wprime = wprime - wmean[indt,zz]
- 
-    vehfmean[indt,zz] = np.mean(tprime*wprime)
-    tprimemean[indt,zz] = np.mean(tprime)
-    wprimemean[indt,zz] = np.mean(wprime)
+r_cp = r_d / cp
+grav = g
 
 
 
-#  ###########
-#  ## method 1: potential temperature profile
-#  diff = np.abs(tmean[indt,2:]-tmean[indt,1])
-#  wheremin = np.argmin(diff) + 2
-#  #print indt, tt
-#  pblh1[indt] = np.mean(geop[tt,wheremin,:,:]) / grav #Height of the geopotential of the boundary layer
- 
-  ###########
-  ## method 2: minimum of vertical eddy heat flux
-  wheremin = np.argmin(vehfmean[indt,:]) 
-  pblh2[indt] = 1.1*np.mean(geop[tt,wheremin,:,:]) / grav
-  ### sometimes spurious values caused by GW
-  #diff = 100.*np.abs(pblh2[indt]-pblh1[indt])/pblh1[indt]
-  #if diff > 33.: pblh2[indt] = pblh1[indt]
+if recalculate:
 
-#  ###########
-#  ## method 3: convective motions
-#  diff = np.abs(wmax[indt,8:]-np.max(wmax[indt,:]/3.))
-#  wheremin = np.argmin(diff) + 8
-#  pblh3[indt] = np.mean(geop[tt,wheremin,:,:]) / grav
-#  ## sometimes spurious values caused by GW
-#  diff = 100.*np.abs(pblh3[indt]-pblh1[indt])/pblh1[indt]
-#  if diff > 33.: pblh3[indt] = pblh1[indt]
+    ## dimensions
+    foo1,foo2,foo3,z,t = pp(file=files[0],var="T",x=0,y=0).getfd()
+    nz,nt = z.size,t.size
+    nf = len(files)
+    print nf
+    ntt = nt*nf
+    
+    ## arrays for time series
+    tprimemean,tmean = np.zeros([ntt,nz]),np.zeros([ntt,nz])
+    wprimemean,wmean = np.zeros([ntt,nz]),np.zeros([ntt,nz])
+    wmax = np.zeros([ntt,nz])
+    vehfmean = np.zeros([ntt,nz])
+    pblh,pblh1,pblh2,pblh3 = np.zeros(ntt),np.zeros(ntt),np.zeros(ntt),np.zeros(ntt)
+    tsurfmean = np.zeros([ntt])
+    
+    pl = ppplot.plot1d() # plot of the boundary layer height time evolution
+    
+    
+    
+    ## loop
+    indt = -1
+    
+    for ff in files:
+    
+     print "### PROCESSING FILE:",ff
+     print "### --> T"
+     t = pp(file=ff,var="T").getf() + t0
+     print "### --> W"
+     w = pp(file=ff,var="W").getf()
+     print "### --> PHTOT"
+     geop = pp(file=ff,var="PHTOT").getf()
+     geop = geop - np.mean(geop[:,0,:,:])
+     print "### --> MARS_TSURF"
+     tsurf = pp(file=ff,var="MARS_TSURF").getf()
+     
+     for tt in range(nt):
+    
+      indt = indt + 1
+    
+      tsurfmean[indt] = np.mean(tsurf[tt,:,:])
+     
+      for zz in range(nz):
+        #print tt, indt
+        tprime = t[tt,zz,:,:]
+        tmean[indt,zz] = np.mean(tprime)
+        tprime = tprime - tmean[indt,zz]
+    
+        wprime = w[tt,zz,:,:]
+        wmean[indt,zz] = np.mean(wprime)
+        wmax[indt,zz] = np.max(np.abs(wprime))
+        wprime = wprime - wmean[indt,zz]
+     
+        vehfmean[indt,zz] = np.mean(tprime*wprime)
+        tprimemean[indt,zz] = np.mean(tprime)
+        wprimemean[indt,zz] = np.mean(wprime)
+    
+      ###########
+      ## method 1: potential temperature profile
+      #diff = np.abs(tmean[indt,2:]-tmean[indt,1])
+      #wheremin = np.argmin(diff) + 2
+      reft = 0.5*(tsurfmean[indt]+tmean[indt,0]) 
+      reft = tmean[indt,0]
+      diff = np.abs(tmean[indt,1:]-reft)
+      wheremin = np.argmin(diff) + 1
+      #print indt, tt
+      pblh1[indt] = np.mean(geop[tt,wheremin,:,:]) / grav #Height of the geopotential of the boundary layer
+      pblh[indt] = pblh1[indt]
+      nnn = 1
+    
+      ###########
+      ## method 2: minimum of vertical eddy heat flux
+      wheremin = np.argmin(vehfmean[indt,:]) 
+      pblh2[indt] = 1.1*np.mean(geop[tt,wheremin,:,:]) / grav
+      ## sometimes spurious values caused by GW
+      diff = 100.*np.abs(pblh2[indt]-pblh1[indt])/pblh1[indt]
+      if diff > limpercent: 
+          pblh2[indt] = np.nan
+      else:
+          pblh[indt] = pblh[indt]+pblh2[indt]
+          nnn = nnn+1
+    
+      ###########
+      ## method 3: convective motions
+      diff = np.abs(wmax[indt,8:]-np.max(wmax[indt,:]/3.))
+      wheremin = np.argmin(diff) + 8
+      pblh3[indt] = np.mean(geop[tt,wheremin,:,:]) / grav
+      ## sometimes spurious values caused by GW
+      diff = 100.*np.abs(pblh3[indt]-pblh1[indt])/pblh1[indt]
+      if diff > limpercent: 
+          pblh3[indt] = np.nan
+      else:
+          pblh[indt] = pblh[indt]+pblh3[indt]
+          nnn = nnn+1
+   
+      ########### 
+      pblh[indt] = pblh[indt]/nnn
+    
+    
+    
+    ### remove small or negative values 
+    #pblh = pblh[pblh > 100.]
+    
+    ## compute mean height
+    altitude = np.mean(np.mean(np.mean(geop,axis=3),axis=2),axis=0)/grav
+    altitude = altitude[0:nz]
+   
+    ## compute time axis 
+    nnn = np.size(pblh1)
+    xaxis = startlt + nfilespinup*foutput*noutput/titanhour + foutput*np.arange(nnn)/titanhour
+    
+    
+    
+    file1=open('pblh1.txt','w')
+    for val in pblh1:
+      file1.write("%8.3f\n"%(val))
+    file1.close()
+    
+    file2=open('pblh2.txt','w')
+    for val in pblh2:
+      file2.write("%8.3f\n"%(val))
+    file2.close()
+    
+    file3=open('pblh3.txt','w')
+    for val in pblh3:
+      file3.write("%8.3f\n"%(val))
+    file3.close()
+    
+    file4=open('xaxis.txt','w')
+    for val in xaxis:
+      file4.write("%8.3f\n"%(val))
+    file4.close()
+    
+    file5=open('pblh.txt','w')
+    for val in pblh:
+      file5.write("%8.3f\n"%(val))
+    file5.close()
+    
 
-#  pblh[indt] = np.mean([pblh1[indt],pblh2[indt],pblh3[indt]])
-  pblh[indt] = pblh2[indt]
+else:
 
-### remove small or negative values 
-#pblh = pblh[pblh > 100.]
-
-## compute mean height
-altitude = np.mean(np.mean(np.mean(geop,axis=3),axis=2),axis=0)/grav
-altitude = altitude[0:nz]
-
-file1=open('pbl.txt','w')
-for val in pblh:
-  file1.write("%8.3f\n"%(val))
-file1.close()
+    pass
 
 
-## 1 Titan day == nearly 16 Earth days
-## 1 Titan hour == 16/24 Earth days == 3600*24*16/24 == 3600*16
-pl = ppplot.plot1d() # plot of the boundary layer height time evolution
-pl.f = pblh
-pl.makeshow()
-pl = ppplot.plot2d() # shade of the vertical eddy heat flux time evolution
-pl.f = np.transpose(vehfmean[30:,:])*1e5
-pl.y = altitude/1000.
-nnn = np.shape(pl.f)[1]
-print nnn
-pl.x = np.arange(nnn)*400./(3600.*16.)
-pl.vmax = 15
-pl.vmin = -pl.vmax
-pl.div = 30
-pl.fmt = '%.0f'
-pl.ymax = 3.
-pl.ylabel = "altitude (km)"
-pl.xlabel = "simulated time (Titan hours)"
-pl.nyticks = 10
-pl.colorbar = "seismic"
-pl.units = r'10$^{-5}$ K m s$^{-1}$'
 
-#pl.makeshow()
-pl.makesave(mode="png",filename="vehf")
-
+#####
+#pl = ppplot.plot2d() # shade of the vertical eddy heat flux time evolution
+#pl.f = np.transpose(vehfmean[:,:])*1e5
+#pl.y = altitude/1000.
+#pl.x = xaxis
+#pl.vmax = 15
+#pl.vmin = -pl.vmax
+#pl.div = 30
+#pl.fmt = '%.0f'
+#pl.ymax = 3.
+#pl.ylabel = "altitude (km)"
+#pl.xlabel = "simulated Titan local time (hours)"
+#pl.nyticks = 10
+#pl.colorbar = "seismic"
+#pl.units = r'10$^{-5}$ K m s$^{-1}$'
+#
+##pl.makeshow()
+#pl.makesave(mode="png",filename="vehf")
+#
---
+++ pblh.py
@@ -0,0 +1,63 @@
+#! /usr/bin/env python
+import ppplot
+import numpy as np
+import ppcompute
+  
+pblh1 = np.loadtxt("pblh1.txt")
+pblh2 = np.loadtxt("pblh2.txt")
+pblh3 = np.loadtxt("pblh3.txt")
+pblh = np.loadtxt("pblh.txt")
+xaxis = np.loadtxt("xaxis.txt")
+
+## correcting case with no mixing layer
+## ... (index 0 cannot be caught in procedure)
+#w = pblh1 < 100. ; pblh1[w] = 0.
+#w = pblh < 100. ; pblh[w] = 0.
+
+win=20
+#win=0
+spblh = ppcompute.smooth1d(pblh,window=win)
+spblh1 = ppcompute.smooth1d(pblh1,window=win)
+sxaxis = ppcompute.smooth1d(xaxis,window=win)
+
+w = np.isfinite(pblh2)
+spblh2 = ppcompute.smooth1d(pblh2[w],window=win)
+sxaxis2 = ppcompute.smooth1d(xaxis[w],window=win)
+
+w = np.isfinite(pblh3)
+spblh3 = ppcompute.smooth1d(pblh3[w],window=win)
+sxaxis3 = ppcompute.smooth1d(xaxis[w],window=win)
+
+ppplot.changefont(12)
+fig = ppplot.figuref(x=12,y=8)
+pl = ppplot.plot1d(fig=fig) # plot of the boundary layer height time evolution
+pl.ymin = 0.
+pl.ymax = 5000.
+pl.f = spblh1
+pl.x = sxaxis
+pl.linestyle = ""
+pl.marker = "x"
+pl.ylabel = "PBL mixing height (m)"
+pl.xlabel = "Titan local time (hours)"
+pl.legend = "potential temperature profile"
+pl.color = "c"
+pl.make()
+pl.f = spblh2
+pl.x = sxaxis2
+pl.legend = "eddy heat flux top inversion"
+pl.color = "m"
+pl.make()
+pl.f = spblh3
+pl.x = sxaxis3
+pl.legend = "plumes' vertical velocities"
+pl.color = "y"
+pl.make()
+pl.f = spblh
+pl.x = sxaxis
+pl.linestyle = '-'
+pl.marker = ""
+pl.color = "k"
+pl.legend = "3-method average"
+pl.makeshow()
+#pl.makesave(mode="png",filename="pblh")
+
