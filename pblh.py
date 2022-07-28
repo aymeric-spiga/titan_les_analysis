6c5b3b8eeb7d561f2982c5e36404fe963a0456d6
Aymeric Spiga <aymeric.spiga@upmc.fr>
2022-07-28 18:36:23 +0200

    setting up appropriate plot limits

--- pblh.py
+++ pblh.py
@@ -34,15 +34,15 @@ spblh3 = ppcompute.smooth1d(pblh3[w],window=win)
 sxaxis3 = ppcompute.smooth1d(xaxis[w],window=win)
 
 ppplot.changefont(12)
-fig = ppplot.figuref(x=16,y=6)
+fig = ppplot.figuref(x=12,y=8) ## 16 6
 pl = ppplot.plot1d(fig=fig) # plot of the boundary layer height time evolution
 ##########################
-#pl.ymin = 0.
-#pl.ymax = 4750.
-#pl.nyticks = 19
-#pl.xmin = 7.5
-#pl.xmax = 19.4
-#pl.nxticks = 24
+pl.ymin = 0.
+pl.ymax = 3200.
+pl.nyticks = 32
+pl.xmin = 6
+pl.xmax = 24
+pl.nxticks = 20
 ##########################
 pl.f = spblh1
 pl.x = sxaxis
@@ -52,6 +52,7 @@ pl.ylabel = "PBL mixing height (m)"
 pl.xlabel = "Local time (Titan hours)"
 pl.legend = "potential temperature profile"
 pl.color = "c"
+#pl.modx = 24
 pl.make()
 pl.f = spblh2
 pl.x = sxaxis2
