4b586c2cc32a4fd1527e5c3ad77ee993474c0392
Aymeric Spiga <aymeric.spiga@upmc.fr>
2022-09-15 09:13:23 +0200

    adding horizontal plot

---
+++ horiz_plot.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+
+pp.py \
+ ../wrfout_d01_9999-01-07_11\:33\:21 \
+ --xp 8 --yp 6 -O png \
+ -v HFX -o horiz_HFX \
+ -t 0 \
+ -C magma \
+ --xcoeff 0.1 --ycoeff 0.1 \
+ --xlabel "west-east axis (km)" --ylabel "south-north axis (km)" \
+ -N 0 -M 2 -F '%.1f' -D 40 
+
+pp.py \
+ ../wrfout_d01_9999-01-07_11\:33\:21 \
+ --xp 8 --yp 6 -O png \
+ -v USTM -o horiz_USTM \
+ -t 0 \
+ -C cividis \
+ --xcoeff 0.1 --ycoeff 0.1 \
+ --xlabel "west-east axis (km)" --ylabel "south-north axis (km)" \
+ --mult 100 -U 'cm s$^{-1}$' \
+ -N 0 -M 4 -F '%.1f' -D 40
+
